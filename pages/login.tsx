59b31c08a0533452f1918711225dde1dfe8c6e25
pauxiel <paul.scholes50@yahoo.com>
2022-07-11 11:06:11 +0100

    add google authentication and hide env

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,7 @@
 import { getSession, signIn } from 'next-auth/react'
-
+import { Box, VStack, Button, chakra, FormLabel, Input } from '@chakra-ui/react'
+import { BsGithub, BsGoogle } from 'react-icons/bs'
+import { useState } from 'react'
 
 export async function getServerSideProps(context) {
   const session = await getSession(context)
@@ -18,9 +20,36 @@ export async function getServerSideProps(context) {
   }
 }
 
+const providers = [
+  { name: 'github', Icon: BsGithub },
+  { name: 'google', Icon: BsGoogle },
+]
+
 function Login() {
+  const [email, setEmail] = useState<string>('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!email) return false
+
+    signIn('email', { email, redirect: false })
+  }
   return (
     <>
+      <Box>
+        <chakra.form onSubmit={handleSubmit}>
+          <FormLabel>Email Address</FormLabel>
+          <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+          <Button type="submit">Get Started</Button>
+        </chakra.form>
+        <VStack>
+          {providers.map(({ name, Icon }) => (
+            <Button key={name} leftIcon={<Icon />} onClick={() => signIn(name)}>
+              Sign in with {name}
+            </Button>
+          ))}
+        </VStack>
+      </Box>
       <button type="button" onClick={() => signIn('github')} className="">
         Github
       </button>
