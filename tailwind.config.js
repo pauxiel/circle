5003b75e119276e6051d0b469e7b6827cc91f9f4
iroatu08 <iroatu08@gmail.com>
2022-07-25 23:02:09 +0100

    home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./asset/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit",]
+      }
+    },
   },
   plugins: [],
 }
