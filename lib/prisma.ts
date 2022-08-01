9f6d7dac4f65e1436f0f2cbccd12f592b1e92ef5
pauxiel <paul.scholes50@yahoo.com>
2022-08-01 14:13:14 +0100

    jjjo

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from '../generated/client'
+// import { PrismaClient } from '../generated/client'
 
 let prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
-export default prisma;
+export default prisma
